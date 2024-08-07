13ef1a99ed14e10e872a6b165c9ce611564bdd5f
logulogu961 <logeshthala000@gmail.com>
2024-08-07 22:44:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const box  = document.getElementById("box");
+const code = document.getElementById("code");
+const btn = document.getElementById("color");
+
+btn.addEventListener('click',colorGenerate);
+
+function colorGenerate(){
+let values ='0123456789abcdef';
+let color = '#';
+
+
+for(let i=0;i<6;i++){
+    let randomvalue = math.floor(math.random()*16);
+    color  += values[randomvalue];
+}
+
+code.innerHTML = color;
+box.style.backgroundColor = color;
+}
